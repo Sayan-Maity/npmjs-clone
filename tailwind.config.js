0c49e1c3bb1dc7f7c12b3a644570ea779c4c0a57
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2024-09-23 02:38:42 +0530

    feat/ initial setup and assets

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        '2xl': '1500px'
+      }
+    },
+  },
+  plugins: [],
+}
